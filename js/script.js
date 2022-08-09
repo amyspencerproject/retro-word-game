5faaa3c21a3142b1503c25f2eabd89824c7f2da1
amyspencerproject <amy@spencerproject.com>
2022-08-09 13:13:01 -0600

    Added if statement to updateProgress to preserve spaces in cocktail after a letter in guessed

--- js/script.js
+++ js/script.js
@@ -32,9 +32,16 @@ let remainingGuesses = 8;
 
 const getCocktail = async function () {
     const response = await fetch(
-        " https://cors-anywhere.herokuapp.com/www.thecocktaildb.com/api/json/v1/1/random.php"
+        "https://cors-anywhere.herokuapp.com/www.thecocktaildb.com/api/json/v1/1/random.php"
+    //    "www.thecocktaildb.com/api/json/v1/1/random.php"
     );
     const data = await response.json();
+    // const parsedData = JSON.parse(data);
+    // console.log(parsedData);
+    // console.log(JSON.parse(data));
+    const raw = {"drinks":[{"idDrink":"178362","strDrink":"Vodka Slime","strDrinkAlternate":null,"strTags":null,"strVideo":null,"strCategory":"Cocktail","strIBA":null,"strAlcoholic":"Alcoholic","strGlass":"Highball glass","strInstructions":"Fill glass with ice. Add vodka, 7-up then finish with the lime juice.","strInstructionsES":null,"strInstructionsDE":null,"strInstructionsFR":null,"strInstructionsIT":null,"strInstructionsZH-HANS":null,"strInstructionsZH-HANT":null,"strDrinkThumb":"https:\/\/www.thecocktaildb.com\/images\/media\/drink\/apex461643588115.jpg","strIngredient1":"Sprite","strIngredient2":"Lime Juice","strIngredient3":"Vodka","strIngredient4":null,"strIngredient5":null,"strIngredient6":null,"strIngredient7":null,"strIngredient8":null,"strIngredient9":null,"strIngredient10":null,"strIngredient11":null,"strIngredient12":null,"strIngredient13":null,"strIngredient14":null,"strIngredient15":null,"strMeasure1":"1 cup","strMeasure2":"1\/2 shot","strMeasure3":"1 1\/2 shot","strMeasure4":"","strMeasure5":"","strMeasure6":"","strMeasure7":"","strMeasure8":null,"strMeasure9":null,"strMeasure10":null,"strMeasure11":null,"strMeasure12":null,"strMeasure13":null,"strMeasure14":null,"strMeasure15":null,"strImageSource":"https:\/\/giggledam.com\/wp-content\/uploads\/2020\/07\/4909D8D9-F78E-4391-B7A2-B383CA57D69F.jpeg","strImageAttribution":null,"strCreativeCommonsConfirmed":"No","dateModified":null}]};
+    console.log(raw);
+    console.log(raw.drinks[0].strDrink)
     console.log(data.drinks[0].strDrink);
     cocktail = data.drinks[0].strDrink;
     placeholder(cocktail);
@@ -45,12 +52,12 @@ getCocktail();
 
 
 //Function to display ● place-holders for each letter of word in play
-const placeholder = function (word) {
+const placeholder = function (cocktail) {
     const placeholderLetters = [];
-    for (const letter of word) {
-        // console.log(letter);
-        placeholderLetters.push("●");
-        //console.log(placeholderLetters);
+    for (const letter of cocktail) {
+        if (letter == " ") {
+            placeholderLetters.push(" ");
+        } else placeholderLetters.push("●");
     }
     wordInProgress.innerText = placeholderLetters.join("");
 };
@@ -79,7 +86,7 @@ const validateInput = function(input) {
     } else if (input.length > 1 ) {
         playerMessage.innerText = "Only enter a single letter as a guess";
     } else if (!input.match(acceptedLetter)) {
-        playerMessage.innerText = "Guess on a single letter from A to Z";
+        playerMessage.innerText = "Guess a single letter from A to Z";
     } else {
         return input
     }
@@ -112,36 +119,36 @@ const displayGuess = function () {
 };
 
 const updateProgress = function (guessedLetters) {
-    const wordUpper = word.toUpperCase(); //make word all uppercase to match guesses
+    const wordUpper = cocktail.toUpperCase(); //make word all uppercase to match guesses
     const wordArray = wordUpper.split(""); //get word string into an array form
     //compare guessed letters to wordArray 
     const revealWord = [];
     for (const letter of wordArray){
-    if (guessedLetters.includes(letter)) {
-        revealWord.push(letter);
-        //replace placeholder bullets with letter
-        } else {
-        revealWord.push("●");
-        }
+        if (letter == " ") {
+            revealWord.push(" ");
+        } else if (guessedLetters.includes(letter)) {
+            revealWord.push(letter);} else {
+                revealWord.push("●")
+            }
     }
-    // console.log(revealWord);
     wordInProgress.innerText = revealWord.join("");
     checkWin();
 };
 
+
 //counldn't decide between guess or goodguess but this can be run only if guess is a good guess just like makeGuess()
 const countGuesses = function (guess) {
     guess = guess.toUpperCase();
-    wordUpper = word.toUpperCase();
+    wordUpper = cocktail.toUpperCase();
     if (!wordUpper.includes(guess)) {
-        playerMessage.innerText = `Sorry the word does not contain ${guess}. Try again!`;
+        playerMessage.innerText = `Sorry this cocktail does not contain ${guess}. Try again!`;
         remainingGuesses -= 1;
     } else {
-        playerMessage.innerText = `Good guess! The word contains ${guess}`;
+        playerMessage.innerText = `Good guess! This cocktail contains ${guess}`;
     }
 
     if (remainingGuesses === 0 ) {
-        playerMessage.innerHTML = `<p class="highlight" > Game over! The word was ${word}</p>`;
+        playerMessage.innerHTML = `<p class="highlight" > Game over! The cocktail was ${word}</p>`;
         remainingSpan.innerText = "0 guesses";
         startOver();
     } else if (remainingGuesses === 1) {
@@ -152,9 +159,9 @@ const countGuesses = function (guess) {
 };
 
 const checkWin = function () {
-    if (word.toUpperCase() === wordInProgress.innerText) {
+    if (cocktail.toUpperCase() === wordInProgress.innerText) {
         playerMessage.classList.add("win");
-        playerMessage.innerHTML = `<p class="highlight" >Good Job! You have correctly guessed the word.</p>`;
+        playerMessage.innerHTML = `<p class="highlight" >Cheers! You have correctly guessed the cocktail.</p>`;
         startOver();
     }
 };
